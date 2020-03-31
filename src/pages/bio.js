ee493bd03e72de581011f2031bce665e3ca7121c
keingsw <dev.keingsw@gmail.com>
2020-03-31 15:17:33 +0800

    feat(bio): create Bio Page

--- src/pages/bio.js
+++ src/pages/bio.js
@@ -1,7 +1,55 @@
 import React from 'react';
+import { RichText } from 'prismic-reactjs';
 
 import Layout from '../components/Layout';
+import Slices from '../components/Slices';
 
-export default () => {
-  return <Layout title="Bio">Bio</Layout>;
+export default ({ data }) => {
+  const bio = data.prismic.allBios.edges.slice(0, 1).pop();
+  if (!bio) return null;
+
+  const { title, body } = bio.node;
+  const titleText = RichText.asText(title);
+  return (
+    <Layout title={titleText} pageTitle={titleText}>
+      <Slices body={body} />
+    </Layout>
+  );
 };
+
+export const query = graphql`
+  {
+    prismic {
+      allBios {
+        edges {
+          node {
+            body {
+              ... on PRISMIC_BioBodyImage {
+                type
+                label
+                primary {
+                  image
+                }
+              }
+              ... on PRISMIC_BioBodyText {
+                type
+                label
+                primary {
+                  text
+                }
+              }
+              ... on PRISMIC_BioBodyEmbed {
+                type
+                label
+                primary {
+                  embed
+                }
+              }
+            }
+            title
+          }
+        }
+      }
+    }
+  }
+`;
