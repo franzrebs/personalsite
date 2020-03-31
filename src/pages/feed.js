c3dab8302fc100c3a8a5d5abecda3700409f7ef9
keingsw <dev.keingsw@gmail.com>
2020-03-31 15:17:46 +0800

    feat(feed): create Feed Page

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,5 +1,55 @@
 import React from 'react';
+import { RichText } from 'prismic-reactjs';
 
 import Layout from '../components/Layout';
+import Slices from '../components/Slices';
 
-export default () => <Layout title="Feed">Feed</Layout>;
+export default ({ data }) => {
+  const feed = data.prismic.allFeeds.edges.slice(0, 1).pop();
+  if (!feed) return null;
+
+  const { title, body } = feed.node;
+  const titleText = RichText.asText(title);
+  return (
+    <Layout title={titleText} pageTitle={titleText}>
+      <Slices body={body} />
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  {
+    prismic {
+      allFeeds {
+        edges {
+          node {
+            body {
+              ... on PRISMIC_FeedBodyImage {
+                type
+                label
+                primary {
+                  image
+                }
+              }
+              ... on PRISMIC_FeedBodyText {
+                type
+                label
+                primary {
+                  text
+                }
+              }
+              ... on PRISMIC_FeedBodyEmbed {
+                type
+                label
+                primary {
+                  embed
+                }
+              }
+            }
+            title
+          }
+        }
+      }
+    }
+  }
+`;
