9a280df16a4ee866f31f96bd7b10da8a1d404869
franzrebs <62796254+franzrebs@users.noreply.github.com>
2021-04-30 20:14:25 +0800

    Updated text in maintenance mode

--- src/components/ComingSoon/ComingSoon.js
+++ src/components/ComingSoon/ComingSoon.js
@@ -5,6 +5,6 @@ import styles from './styles';
 
 export default ({ text }) => (
   <div css={styles.root}>
-    <h1 css={styles.text}>{text ? text : 'Coming soon...'}</h1>
+    <h1 css={styles.text}>{text ? text : 'So, I kind of dropped the ball on this...'}</h1>
   </div>
 );
